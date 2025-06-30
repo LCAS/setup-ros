b0628bd6e82a290efb07154c5abdc79853675367
Marc Hanheide <marc@hanheide.net>
2025-06-30 09:59:11 +0100

    Add ca-certificates to the list of packages installed for worker system configuration

--- src/setup-ros-linux.ts
+++ src/setup-ros-linux.ts
@@ -86,7 +86,13 @@ async function configOs(): Promise<void> {
 	await utils.exec("sudo", ["apt-get", "update"]);
 
 	// Install tools required to configure the worker system.
-	await apt.runAptGetInstall(["curl", "gnupg2", "locales", "lsb-release"]);
+	await apt.runAptGetInstall([
+		"curl",
+		"gnupg2",
+		"locales",
+		"lsb-release",
+		"ca-certificates",
+	]);
 
 	// Select a locale supporting Unicode.
 	await utils.exec("sudo", ["locale-gen", "en_US", "en_US.UTF-8"]);
